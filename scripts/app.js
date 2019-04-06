132f5f98a869e56a710cc162e6bea821a5aad061
JanezTheProgramer <43420276+JanezTheProgramer@users.noreply.github.com>
2019-04-07 01:25:47 +0200

    transfom drag

--- scripts/app.js
+++ scripts/app.js
@@ -6,14 +6,14 @@ window.$ = window.jQuery = require('jquery');
 const electron = require('electron');
 const { ipcRenderer } = electron;
 const {
-        getConfiguration,
-        getTheme,
-        user_getEverything,
-        setUserName,
-        setConfiguration,
-        setTheme,
-        user_setEverything
-    } = require('../scripts/sqliteQuery'),
+    getConfiguration,
+    getTheme,
+    user_getEverything,
+    setUserName,
+    setConfiguration,
+    setTheme,
+    user_setEverything
+} = require('../scripts/sqliteQuery'),
     path = require('path'),
     moment = require('moment'),
     weather = require('weather-js'),
@@ -24,11 +24,56 @@ const {
     { determineTheme } = require('../scripts/theme'),
     AColorPicker = require('a-color-picker');
 
-window.drag = {
-    isDown: false,
-    iX: null,
-    iY: null
-}; // object for element dragging properties
+window.dragActivity = {
+    active: false,
+    target: null
+};
+
+$.fn.hasAttr = function (name) {
+    return this.attr(name) !== undefined && this.attr(name) != true;
+};
+
+const dragStart = e => {
+    $('.box-window').css('z-index', '0');
+    dragActivity.active = true;
+    let element = $(e.target).closest('.box-window').get(0).id;
+    if (typeof $(e.target).attr('drag') === typeof undefined) return;
+    dragActivity.target = components[element.substr(0, element.length - 11)];
+    if ($(document.getElementById(dragActivity.target.id)).hasAttr("fscreen")) {
+        console.log("tlke", document.getElementById('notes-box-window'));
+        dragActivity.target = null;
+        dragActivity.active = false;
+        return;
+    }
+
+    document.getElementById(dragActivity.target.id).style.zIndex = '1';
+    $(`#${dragActivity.target.id}`).css("border", `.2vw solid ${
+        $('.box-window-top').css('background-color')
+    }`);
+    dragActivity.target.initialX = e.clientX - dragActivity.target.xOffset;
+    dragActivity.target.initialY = e.clientY - dragActivity.target.yOffset;
+};
+
+const drag = e => {
+    if (!dragActivity.active || !dragActivity.target) return;
+    e.preventDefault();
+    dragActivity.target.currentX = e.clientX - dragActivity.target.initialX;
+    dragActivity.target.currentY = e.clientY - dragActivity.target.initialY;
+    dragActivity.target.xOffset = dragActivity.target.currentX;
+    dragActivity.target.yOffset = dragActivity.target.currentY;
+    document.getElementById(dragActivity.target.id).style.transform = (`
+        translate(${dragActivity.target.currentX}px, ${dragActivity.target.currentY}px)
+    `);
+};
+
+const dragEnd = () => {
+    if (!dragActivity.active || !dragActivity.target) return;
+    dragActivity.target.initialX = dragActivity.target.currentX;
+    dragActivity.target.initialY = dragActivity.target.currentY;
+    dragActivity.active = false;
+    dragActivity.target = null;
+    $(`.box-window`).css("border", "none");
+};
 
 window.Platform = {
     css_id: 'alternate-css',
@@ -60,68 +105,77 @@ window.Platform = {
             setTimeout(() => $(document.getElementById('platformDiv')).fadeIn(200), 300);
         }).catch(err => console.log(err));
     }
-}
+};
 
 window.components = {
     games: {
         id: 'games-box-window', file: 'games.html', enabled: true,
-        tooltip: 'eXo-games', defHeight: '50vh'
+        tooltip: 'eXo-games', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, calculator: {
         id: 'calculator-box-window', file: 'calculator.html', enabled: true,
-        tooltip: 'eXo-calculator', defHeight: '50vh'
+        tooltip: 'eXo-calculator', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, notes: {
         id: 'notes-box-window', file: 'notes.html', enabled: true,
-        tooltip: 'eXo-notes', defHeight: '50vh'
+        tooltip: 'eXo-notes', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, music: {
         id: 'music-box-window', file: 'music.html', enabled: true,
-        tooltip: 'eXo-music', defHeight: '50vh'
+        tooltip: 'eXo-music', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, video: {
         id: 'video-box-window', file: 'videoPlayer.html', enabled: true,
-        tooltip: 'eXo-video', defHeight: '50vh'
+        tooltip: 'eXo-video', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, weather: {
         id: 'weather-box-window', file: 'weather.html', enabled: true,
-        tooltip: 'eXo-weather', defHeight: '50vh'
+        tooltip: 'eXo-weather', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, maps: {
         id: 'maps-box-window', file: 'maps.html', enabled: true,
-        tooltip: 'eXo-maps', defHeight: '50vh'
+        tooltip: 'eXo-maps', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, photoEditor: {
         id: 'photoEditor-box-window', file: 'photoEditor.html', enabled: true,
-        tooltip: 'eXo-photoEditor', defHeight: '50vh'
+        tooltip: 'eXo-photoEditor', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, sysInfo: {
         id: 'sysInfo-box-window', file: 'sysInfo.html', enabled: true,
-        tooltip: 'eXo-sysInfo', defHeight: '50vh'
+        tooltip: 'eXo-sysInfo', defHeight: '50vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }, sysControl: {
         id: 'sysControl-box-window', file: 'sysControl.html', enabled: true,
-        tooltip: 'exo-sysControl', defHeight: '35vh'
+        tooltip: 'exo-sysControl', defHeight: '35vh', currentX: null,
+        currentY: null, initialX: null, initialY: null, yOffset: 0,
+        xOffset: 0
     }
 };
 
-/// 3 global drag functions in vanilla JS (old school)
-
-const downTrigger = () => {
-    drag.isDown = true;
-    drag.iX = event.clientX - event.offsetX;
-    drag.iY = event.clientY - event.offsetY;
-};
-
-const moveTrigger = () => {
-    if (!drag.isDown) return;
-    event.currentTarget.parentNode.parentNode.style.top = `${event.clientY - drag.iY}px`;
-    event.currentTarget.parentNode.parentNode.style.left = `${event.clientX - drag.iX}px`;
-    event.currentTarget.parentNode.parentNode.style.zIndex = '1';
-};
-
-const leaveTrigger = () => drag.isDown = false;
-
 $(document).ready(() => {
+    let container = document.getElementById('content');
+
+    container.onmousedown = dragStart;
+    container.onmousemove = drag;
+    //$('.box-window').mouseleave(dragEnd);
+    document.onmouseup = dragEnd;
     //automated functions 
     setTimeout(() => $('body').fadeIn(500), 1000);
     (() => setInterval(() => $(document.getElementById('clock')).text(`${moment().format('LT')}`), 60000))();
     (() => {
         Platform.onLoad();
         if (!navigator.onLine)
-            for (let key in ['sysControl', 'maps', 'weather'])
-                components[key].enabled = false;
+            $.each(['maps', 'weather'], (index, value) =>
+                components[value].enabled = false);
     })();
 
     $(document.getElementById('clock')).text(moment().format('LT'));
@@ -176,8 +230,6 @@ $(document).ready(() => {
         else if (e.which == 82) self.location.assign(location); //restart app on 'r' pressed
     });
 
-    $(document).mouseup(() => drag.isDown = false);
-
     $(document).on('click', '.exit', e => closeTargetWindow(e.currentTarget.parentNode.parentNode.id));
 
     $(document).on('mousedown', '.box-window', event => {
@@ -185,12 +237,30 @@ $(document).ready(() => {
         event.currentTarget.style.zIndex = '1';
     });
 
+    const find = (object, id_) => {
+        for (let component in object)
+            if (object[component].id == id_) return component;
+    }
+
+    const resetToDefault = (component_, type) => {
+        type = type || null;
+        component_.currentX = null;
+        component_.currentY = null;
+        component_.initialX = null;
+        component_.initialY = null;
+        component_.yOffset = 0;
+        component_.xOffset = 0;
+        if (!type) return;
+        $(document.getElementById(component_.id)).css({
+            transform: 'translate(0,0)',
+            top: '0',
+            left: '0'
+        });
+    };
+
     $(document).on('mousedown', '.box-window-top .box-window-toggle-fullScreen', e => {
         let targetID = e.currentTarget.parentNode.parentNode.id;
-        const find = object => {
-            for (let component in object)
-                if (object[component].id == targetID) return component;
-        }
+        console.log(components[find(components, targetID)]);
         if (!document.getElementById(targetID)) return;
         //console.log(e.currentTarget.parentNode.parentNode);
         if (['▢', '&#9634;'].includes(e.target.innerHTML)) {
@@ -205,15 +275,19 @@ $(document).ready(() => {
                 top: '8vh',
                 left: '15vw',
                 width: '50vw',
-                height: components[find(components)].defHeight
+                height: components[find(components, targetID)].defHeight
             }, 800);
             document.getElementById('leftNav').setAttribute('toggle', '1');
             e.target.innerHTML = '&#9634;';
+            $(document.getElementById(targetID)).removeAttr('fscreen');
         }
     });
 
-    const goFullScreen = targetID => {
-        $(document.getElementById(targetID)).css({
+    const goFullScreen = async targetID => {
+        let target_element = document.getElementById(targetID),
+            curr_component = components[find(components, targetID)];
+        console.log(target_element.style);
+        $(target_element).css({
             maxHeight: '100vh',
             maxWidth: '100vw'
         });
@@ -223,17 +297,21 @@ $(document).ready(() => {
             opacity: '0'
         });
         document.getElementById('leftNav').setAttribute('toggle', '0');
-        $(document.getElementById(targetID)).animate({
-            top: '0',
-            left: '0',
+        $(target_element).animate({
+            left: `${0 - Number(curr_component.currentX)}px`,
+            top: `${0 - Number(curr_component.currentY)}px`,
             width: '100%',
             height: '100%'
         }, 800);
+        setTimeout(() => resetToDefault(curr_component, true), 900);
+        $(target_element).attr('fscreen', true);
         $(`#${targetID} .box-window-top .box-window-toggle-fullScreen`).html('&minus;');
     }
 
     const closeTargetWindow = (targetID, speed) => {
         speed = speed || 700;
+        resetToDefault(components[find(components, targetID)]);
+        console.log(targetID);
         let obj = $(document.getElementById(targetID));
         if (obj) {
             $(document.getElementById('leftNav')).attr('toggle') == '0' ? $(document.getElementById('leftNav')).animate({
